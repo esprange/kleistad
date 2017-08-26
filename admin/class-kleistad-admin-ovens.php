98d98ace6fc32db0cf3d49bb6088d98e2ed36be1
Eric Sprangers <eric.sprangers@gmail.com>
2017-08-26 23:31:23 +0200

    beperk checks voorlopig op php 5.x

--- admin/class-kleistad-admin-ovens.php
+++ admin/class-kleistad-admin-ovens.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The admin-specific functionality for management of ovens of the plugin.
  *
@@ -9,6 +8,7 @@
  * @package Kleistad
  * @subpackage Kleistad/admin
  */
+
 if ( ! class_exists( 'WP_List_Table' ) ) {
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
@@ -30,59 +30,59 @@ class Ovens_List_Table extends WP_List_Table {
 
 	/**
 	 * Set the defaults for columns
-	 * 
-	 * @param $item - row (key, value array)
-	 * @param $column_name - string (key)
+	 *
+	 * @param array $item			row (key, value array).
+	 * @param string $column_name	key.
 	 * @return HTML
 	 */
 	function column_default( $item, $column_name ) {
-		return $item[ $column_name ];
+		return $item[$column_name];
 	}
 
 	/**
 	 * Render the column naam with the actions
-	 * 
-	 * @param array $item - row (key, value array)
+	 *
+	 * @param array $item			row (key, value array).
 	 * @return HTML
 	 */
 	function column_naam( $item ) {
 		$actions = [
-			'edit' => sprintf( '<a href="?page=ovens_form&id=%s">%s</a>', $item[ 'id' ], 'Wijzigen' ),
+			'edit' => sprintf( '<a href="?page=ovens_form&id=%s">%s</a>', $item['id'], 'Wijzigen' ),
 		];
 
-		return sprintf( '%s %s', $item[ 'naam' ], $this->row_actions( $actions )
+		return sprintf( '%s %s', $item['naam'], $this->row_actions( $actions )
 		);
 	}
 
 	/**
-	 * render the column beschikbaarheid
-	 * 
-	 * @param array $item - row (key, value array)
+	 * Render the column beschikbaarheid
+	 *
+	 * @param array $item			row (key, value array).
 	 * @return HTML
 	 */
 	function column_beschikbaarheid( $item ) {
-		$beschikbaarheid = json_decode( $item[ 'beschikbaarheid' ], true );
+		$beschikbaarheid = json_decode( $item['beschikbaarheid'], true );
 		return implode( ', ', $beschikbaarheid );
 	}
 
 	/**
-	 * return the column titles
-	 * 
+	 * Return the column titles
+	 *
 	 * @return array
 	 */
 	function get_columns() {
 		$columns = [
-			'naam'				 => 'Naam',
-			'kosten'			 => 'Tarief',
-			'beschikbaarheid'	 => 'Beschikbaarheid',
-			'id'				 => 'Id',
+			'naam'				=> 'Naam',
+			'kosten'			=> 'Tarief',
+			'beschikbaarheid'	=> 'Beschikbaarheid',
+			'id'				=> 'Id',
 		];
 		return $columns;
 	}
 
 	/**
-	 * define the sortable columns
-	 * 
+	 * Define the sortable columns
+	 *
 	 * @return array
 	 */
 	function get_sortable_columns() {
@@ -100,25 +100,27 @@ class Ovens_List_Table extends WP_List_Table {
 		global $wpdb;
 		$table_name = $wpdb->prefix . 'kleistad_ovens';
 
-		$per_page = 5; // constant, how much records will be shown per page
+		$per_page = 5; // constant, how much records will be shown per page.
 
-		$columns	 = $this->get_columns();
-		$hidden		 = [];
-		$sortable	 = $this->get_sortable_columns();
+		$columns = $this->get_columns();
+		$hidden	= [];
+		$sortable = $this->get_sortable_columns();
 
 		$this->_column_headers = [ $columns, $hidden, $sortable ];
+		
+		$sql = $wpdb->prepare( "SELECT COUNT(id) FROM $table_name" );
+		$total_items = $wpdb->get_var( $sql );
 
-		$total_items = $wpdb->get_var( "SELECT COUNT(id) FROM $table_name" );
+		$paged = isset( $_REQUEST['paged'] ) ? max( 0, intval( $_REQUEST['paged'] ) - 1 ) : 0;
+		$orderby = (isset( $_REQUEST['orderby'] ) && in_array( $_REQUEST['orderby'], array_keys( $this->get_sortable_columns() ) )) ? $_REQUEST['orderby'] : 'naam';
+		$order = (isset( $_REQUEST['order'] ) && in_array( $_REQUEST['order'], [ 'asc', 'desc' ] )) ? $_REQUEST['order'] : 'asc';
 
-		$paged		 = isset( $_REQUEST[ 'paged' ] ) ? max( 0, intval( $_REQUEST[ 'paged' ] ) - 1 ) : 0;
-		$orderby	 = (isset( $_REQUEST[ 'orderby' ] ) && in_array( $_REQUEST[ 'orderby' ], array_keys( $this->get_sortable_columns() ) )) ? $_REQUEST[ 'orderby' ] : 'naam';
-		$order		 = (isset( $_REQUEST[ 'order' ] ) && in_array( $_REQUEST[ 'order' ], [ 'asc', 'desc' ] )) ? $_REQUEST[ 'order' ] : 'asc';
-
-		$this->items = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $table_name ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged ), ARRAY_A );
+		$sql = $wpdb->prepare( "SELECT * FROM $table_name ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged );
+		$this->items = $wpdb->get_results( $sql, ARRAY_A );
 		$this->set_pagination_args( [
-			'total_items'	 => $total_items, // total items defined above
-			'per_page'		 => $per_page, // per page constant defined at top of method
-			'total_pages'	 => ceil( $total_items / $per_page ) // calculate pages count
+			'total_items'	=> $total_items, // total items defined above.
+			'per_page'		=> $per_page, // per page constant defined at top of method.
+			'total_pages'	=> ceil( $total_items / $per_page ), // calculate pages count.
 		] );
 	}
 
