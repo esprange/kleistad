d86866bb6547ae7511af39ef6e792bd4a2077fa3
Eric Sprangers <eric.sprangers@gmail.com>
2017-08-28 16:55:12 +0200

    first test of class Kleistad_Oven

---
+++ tests/test-ovens.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Kleistad
+ */
+
+/**
+ * Sample test case.
+ */
+class OvensTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_oven() {
+		// Replace this with some actual testing code.
+		$oven = new Kleistad_Oven();
+		$oven->naam = 'test oven';
+		$oven->kosten = 10;
+		$oven_id = $oven->save();
+		$this->assertTrue( $oven_id > 0 );
+	}
+}
