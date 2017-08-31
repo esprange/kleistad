c7ad14f8fef7059d11da24c9f41d1989ab590aa5
Eric Sprangers <eric.sprangers@gmail.com>
2017-08-31 12:13:54 +0200

    uitbreiding testen

--- tests/test-ovens.php
+++ tests/test-ovens.php
@@ -32,6 +32,12 @@ class OvensTest extends WP_UnitTestCase {
 
 		$oven2->kosten = 12;
 		$this->assertEquals( 12, $oven2->kosten );
+
+		$upload_dir      = wp_upload_dir();
+		$transactie_log  = $upload_dir['basedir'] . '/stooksaldo.log';
+
+		Kleistad_Oven::log_saldo( 'test' );
+		$this->assertFileExists( $transactielog );
 	}
 
 	/**
@@ -53,6 +59,57 @@ class OvensTest extends WP_UnitTestCase {
 				$this->assertEquals( 'test ovens ' . intval( $oven->kosten ), $oven->naam );
 			}
 		}
+
+	}
+
+	/**
+	 * Test creation and modification of a reservering.
+	 */
+	function test_reservering() {
+		$user_id = $this->factory->user->create(
+			array(
+				'role' => 'subscriber',
+			)
+		);
+		$datum = time();
+
+		$oven = new Kleistad_Oven();
+		$oven->naam = 'test';
+		$oven->kosten = 5;
+		$oven_id = $oven->save();
+		$reservering1 = new Kleistad_Reservering( $oven_id );
+		$reservering1->datum = $datum;
+		$reservering1->gebruiker_id = $user_id;
+		$reservering1->temperatuur = 999;
+		$reservering1->soortstook = 'test';
+		$reservering1->programma = 1;
+		$reservering1->verdeling = [
+			[
+				'id'     => $user_id,
+				'perc'   => 100,
+			],
+			[
+				'id'     => 0,
+				'perc'   => 0,
+			],
+			[
+				'id'     => 0,
+				'perc'   => 0,
+			],
+			[
+				'id'     => 0,
+				'perc'   => 0,
+			],
+			[
+				'id'     => 0,
+				'perc'   => 0,
+			],
+		];
+		$reservering1->opmerking = 'test remark';
+		$reservering1->save();
+
+		$reservering2 = new Kleistad_Reservering( $oven_id );
+		$this->assertTrue( $reservering2->find( date( 'Y', $datum ), date( 'm', $datum ), date( 'd', $datum ) ) );
 
 	}
 }
