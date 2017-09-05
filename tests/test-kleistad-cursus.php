89d7203707538f4bb3d8b17000dce4c18894b86f
Eric Sprangers <eric.sprangers@gmail.com>
2017-09-05 22:46:21 +0200

    extending tests

--- tests/test-kleistad-cursus.php
+++ tests/test-kleistad-cursus.php
@@ -18,7 +18,7 @@ class KleistadCursusTest extends WP_UnitTestCase {
 		activate_kleistad();
 	}
 	/**
-	 * Test creation and modification of an oven.
+	 * Test creation and modification of a cursus.
 	 */
 	function test_cursus() {
 		$cursus1 = new Kleistad_Cursus();
@@ -53,6 +53,51 @@ class KleistadCursusTest extends WP_UnitTestCase {
 		foreach ( $cursussen_from_store as $cursus ) {
 			if ( substr( $cursus->naam, 0, strlen( $teststring ) ) == $teststring ) {
 				$this->assertEquals( $teststring . $cursus->docent, $cursus->naam, 'naam cursussen not equal' );
+			}
+		}
+
+	}
+
+	/**
+	 * Test creation and modification of an inschrijving.
+	 */
+	function test_inschrijving() {
+		$cursist_id = $this->factory->user->create();
+		$cursus = new Kleistad_Cursus();
+
+		$inschrijving1 = new Kleistad_Inschrijving( $cursist_id, $cursus->id );
+		$inschrijving1->opmerking = 'test inschrijving';
+		$inschrijving_id = $inschrijving1->save();
+		$this->assertTrue( $inschrijving_id > 0, 'save inschrijving no id' );
+
+		$inschrijving2 = new Kleistad_Cursus( $cursist_id, $cursus->id );
+		$this->assertEquals( 'test inschrijving', $inschrijving2->naam, 'naam inschrijving not equal' );
+
+		$inschrijving2->technieken = [ 'techniek1', 'techniek2' ];
+		$this->assertEquals( [ 'techniek1', 'techniek2' ], $inschrijving2->technieken, 'technieken inschrijving not equal' );
+
+	}
+
+	/**
+	 * Test creation and modification of multiple inschrijvingen.
+	 */
+	function test_inschrijvingen() {
+		$cursist_ids = $this->factory->user->create_many( 10 );
+		$cursus = new Kleistad_Cursus();
+
+		$teststring = 'test inschrijvingen';
+		$inschrijvingsen = [];
+		for ( $i = 0; $i < 10; $i++ ) {
+			$inschrijvingen[ $i ] = new Kleistad_Inschrijving( $cursist_ids[ $i ], $cursus->id );
+			$inschrijvingen[ $i ]->opmerking = "$teststring{$cursist_ids[$i]}";
+			$inschrijvingen[ $i ]->save();
+		}
+
+		$inschrijvingen_store = new Kleistad_Inschrijvingen();
+		$inschrijvingen_from_store = $inschrijvingen_store->get();
+		foreach ( $inschrijvingen_from_store as $cursist_id => $inschrijving ) {
+			if ( substr( $inschrijving->opmerking, 0, strlen( $teststring ) ) == $teststring ) {
+				$this->assertEquals( $teststring . $cursist_id, $inschrijving->opmerking, 'naam inschrijvingen not equal' );
 			}
 		}
 
