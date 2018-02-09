b01fc0733de2b43d7f0eaee4d408aa4077fc9006
Eric Sprangers <eric.sprangers@gmail.com>
2018-02-09 19:56:04 +0100

    formatting

--- public/js/kleistad-public-recept_beheer.js
+++ public/js/kleistad-public-recept_beheer.js
@@ -84,7 +84,7 @@
 
             $( '.extra_regel' ).click( function() {
                 var oldRow, newRow;
-                oldRow = $( this ).closest( 'tr' ).prev();;
+                oldRow = $( this ).closest( 'tr' ).prev();
                 newRow = oldRow.clone().find( 'input' ).val( '' ).end();
                 oldRow.after( newRow );
                 return false;
