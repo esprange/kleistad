003ef7de2ec5856e6d4471363646c62337d680df
Eric Sprangers <eric.sprangers@gmail.com>
2018-05-28 14:02:15 +0200

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,7 +13,12 @@ $GLOBALS['wp_tests_options'] = array(
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
