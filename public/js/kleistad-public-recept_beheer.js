15aabb2b692be372d0d9e81235b488c9161c2c32
Eric Sprangers <eric.sprangers@gmail.com>
2018-06-03 15:39:21 +0200

    code style fix

--- public/js/kleistad-public-recept_beheer.js
+++ public/js/kleistad-public-recept_beheer.js
@@ -11,7 +11,7 @@
 				!! ( document.createElement( 'datalist' ) && window.HTMLDataListElement );
 			if ( ! nativedatalist ) {
 				$( 'input[list]' ).each( function() {
-					var availableTags = $( '#' + $ (this ).attr( 'list' ) ).find( 'option' ).map( function() {
+					var availableTags = $( '#' + $( this ).attr( 'list' ) ).find( 'option' ).map( function() {
 						return this.value;
 						}
 					).get();
