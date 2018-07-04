10f128efc3e7bcde933357c56e8b1cdaf4602fb1
Eric Sprangers <eric.sprangers@gmail.com>
2018-07-04 23:03:21 +0200

    Release voorbereiding

--- .phan/config.php
+++ .phan/config.php
@@ -12,43 +12,37 @@ return [
 	// If this is set to null,
 	// then Phan assumes the PHP version which is closest to the minor version
 	// of the php executable used to execute phan.
-	'target_php_version'              => null,
-	'quick_mode'                      => false,
+	'target_php_version'              => '7.2',
+	'quick_mode'                      => true,
 	'backward_compatibility_checks'   => true,
+	'analyze_signature_compatibility' => true,
+	'minimum_severity'                => 0,  // 0 is lowest, 10 is high
+	'allow_missing_properties'        => false,
+	'null_casts_as_any_type'          => false,
+	'null_casts_as_array'             => false,
+	'array_casts_as_null'             => false,
+	'scalar_implicit_cast'            => false,
+	'scalar_implicit_partial'         => [],
 
+
+	'ignore_undeclared_variables_in_global_scope' => false,
 
 	// A list of directories that should be parsed for class and
 	// method information. After excluding the directories
 	// defined in exclude_analysis_directory_list, the remaining
 	// files will be statically analyzed for errors.
 	'directory_list'                  => [
-		"\\xampp\\htdocs\\wordpress",
-		// 'vendor',
-		// 'includes',
-		// 'admin',
-		// 'public',
+		'.',
+		"\\referentie",
 	],
 
 	// A directory list that defines files that will be excluded
 	// from static analysis, but whose class and method
 	// information should be included.
 	'exclude_analysis_directory_list' => [
-		"\\xampp\\htdocs\\wordpress",
-		"\\xampp\\htdocs\\wordpress\\wp-content\\plugins\\kleistad\\vendor\\",
-		"\\xampp\\htdocs\\wordpress\\wp-content\\plugins\\kleistad\\tests\\",
-	// "\\xampp\\htdocs\\",
-	],
-	'include_analysis_directory_list' => [
-		"\\xampp\\htdocs\\wordpress\\wp-content\\plugins\\kleistad",
-	],
-	'exclude_file_list'               => [
-		'bin/',
-		'node_modules/',
-		'tests/',
-		'tmp/',
-		'vendor/',
-		'.phan/',
-		'.vscode/',
+		'vendor',
+		'tests',
+		"\\referentie",
 	],
 
 	// Add any issue types (such as 'PhanUndeclaredMethod')
@@ -58,7 +52,7 @@ return [
 		// 'PhanUndeclaredClassMethod',
 		// 'PhanUndeclaredClassConstant',
 		// 'PhanUndeclaredFunction',
-		'PhanRedefinedExtendedClass',
+		//'PhanRedefinedExtendedClass',
 	],
 
 
