6cdcb024350b17c1c24c01636473519d4c414027
Eric Sprangers <eric.sprangers@gmail.com>
2018-10-21 11:07:12 +0200

    Versie release

--- kleistad.php
+++ kleistad.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Kleistad
  * Plugin URI:        https://github.com/esprange/kleistad
  * Description:       Een plugin voor vereniging Kleistad. Overreserveringen, stooksaldo administratie, cursus adminstratie en keramiek recepten.
- * Version:           4.5.9
+ * Version:           4.5.10
  * Author:            Eric Sprangers
  * Author URI:        https://www.kleistad.nl
  * License:           GPL-3.0+
