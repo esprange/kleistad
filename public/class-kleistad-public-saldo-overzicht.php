f9129818b7ec5cd48da6caf60632c65e5f13b992
Eric Sprangers <eric.sprangers@gmail.com>
2019-03-25 14:54:29 +0100

    Code style

--- public/class-kleistad-public-saldo-overzicht.php
+++ public/class-kleistad-public-saldo-overzicht.php
@@ -26,7 +26,7 @@ class Kleistad_Public_Saldo_Overzicht extends Kleistad_Shortcode {
 	 * @since   4.0.87
 	 */
 	protected function prepare( &$data = null ) {
-		$gebruikers = get_users(
+		$gebruikers      = get_users(
 			[
 				'fields'  => [ 'ID', 'display_name' ],
 				'orderby' => [ 'nicename' ],
@@ -35,7 +35,7 @@ class Kleistad_Public_Saldo_Overzicht extends Kleistad_Shortcode {
 		$data['stokers'] = [];
 		foreach ( $gebruikers as $gebruiker ) {
 			if ( Kleistad_Roles::reserveer( $gebruiker->ID ) ) {
-				$saldo     = new Kleistad_Saldo( $gebruiker->ID );
+				$saldo             = new Kleistad_Saldo( $gebruiker->ID );
 				$data['stokers'][] = [
 					'naam'  => $gebruiker->display_name,
 					'saldo' => number_format_i18n( $saldo->bedrag, 2 ),
