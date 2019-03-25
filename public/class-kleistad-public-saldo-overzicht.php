0697b309962f57424152d824e3f259db709c9c9e
Eric Sprangers <eric.sprangers@gmail.com>
2019-03-25 14:27:03 +0100

    Refactor saldo overzicht

--- public/class-kleistad-public-saldo-overzicht.php
+++ public/class-kleistad-public-saldo-overzicht.php
@@ -32,19 +32,16 @@ class Kleistad_Public_Saldo_Overzicht extends Kleistad_Shortcode {
 				'orderby' => [ 'nicename' ],
 			]
 		);
-		$stokers    = [];
+		$data['stokers'] = [];
 		foreach ( $gebruikers as $gebruiker ) {
 			if ( Kleistad_Roles::reserveer( $gebruiker->ID ) ) {
 				$saldo     = new Kleistad_Saldo( $gebruiker->ID );
-				$stokers[] = [
+				$data['stokers'][] = [
 					'naam'  => $gebruiker->display_name,
 					'saldo' => number_format_i18n( $saldo->bedrag, 2 ),
 				];
 			}
 		}
-		$data = [
-			'stokers' => $stokers,
-		];
 		return true;
 	}
 
