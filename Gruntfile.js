fd653e4b3ddcc902c948b7b3e460219ba643ab6a
esprange <eric.sprangers@gmail.com>
2020-05-12 18:22:40 +0200

    composer optimize in built

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function( grunt ) { // jshint ignore:line
 			'cssmin',
 			'composer:update:no-autoloader',
 			'clean',
-			'composer:dump-autoload',
+			'composer:dump-autoload:optimize',
 			'zip'
 		]
 	);
