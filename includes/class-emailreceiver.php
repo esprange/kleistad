b1e9eae88dc32fe28d20ec77a3a82bb0a94adae5
esprange <eric.sprangers@gmail.com>
2021-02-27 14:07:18 +0100

    Mogelijk oplossing voor connection fail

--- includes/class-emailreceiver.php
+++ includes/class-emailreceiver.php
@@ -46,32 +46,32 @@ class EmailReceiver {
 		);
 		try {
 			$email_ids = $mailbox->searchMailbox( 'UNANSWERED' );
-			foreach ( $email_ids as $email_id ) {
-				$email = $mailbox->getMail( $email_id );
-				$body  = $email->textPlain ?: '<p>bericht tekst kan niet worden weergegeven</p>';
-				if ( $email->textHtml ) {
-					$html = new Html2Text\Html2Text( preg_replace( '/<!--\[if gte mso 9\]>.*<!\[endif\]-->/s', '', $email->textHtml ) );
-					$body = $html->getText() ?: $body;
-				}
-				$verwerk(
-					[
-						'from-name' => isset( $email->fromName ) ? sanitize_text_field( $email->fromName ) : sanitize_email( $email->fromAddress ),
-						'from'      => sanitize_email( $email->fromAddress ),
-						'subject'   => sanitize_text_field( $email->subject ),
-						'content'   => sanitize_textarea_field( $body ),
-					]
-				);
-				$answered[] = $email_id;
-			}
-			if ( ! empty( $answered ) ) {
-				$mailbox->setFlag( $answered, '\\Answered' );
-			}
-			$mailbox->disconnect();
 		} catch ( PhpImap\Exceptions\ConnectionException $ex ) {
 			error_log( 'IMAP fail: ' . $ex->getMessage() ); // phpcs:ignore
 			die();
 		}
-	// phpcs:enable
+		foreach ( $email_ids as $email_id ) {
+			$email = $mailbox->getMail( $email_id );
+			$body  = $email->textPlain ?: '<p>bericht tekst kan niet worden weergegeven</p>';
+			if ( $email->textHtml ) {
+				$html = new Html2Text\Html2Text( preg_replace( '/<!--\[if gte mso 9\]>.*<!\[endif\]-->/s', '', $email->textHtml ) );
+				$body = $html->getText() ?: $body;
+			}
+			$verwerk(
+				[
+					'from-name' => isset( $email->fromName ) ? sanitize_text_field( $email->fromName ) : sanitize_email( $email->fromAddress ),
+					'from'      => sanitize_email( $email->fromAddress ),
+					'subject'   => sanitize_text_field( $email->subject ),
+					'content'   => sanitize_textarea_field( $body ),
+				]
+			);
+			$answered[] = $email_id;
+		}
+		if ( ! empty( $answered ) ) {
+			$mailbox->setFlag( $answered, '\\Answered' );
+		}
+		$mailbox->disconnect();
+		// phpcs:enable
 	}
 
 }
