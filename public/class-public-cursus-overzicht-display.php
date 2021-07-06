c3fa62043fd7ab4d60b6d19820f83ab7897e2c3d
esprange <eric.sprangers@gmail.com>
2021-07-06 10:38:40 +0200

    refactor cursisten

--- public/class-public-cursus-overzicht-display.php
+++ public/class-public-cursus-overzicht-display.php
@@ -23,8 +23,11 @@ class Public_Cursus_Overzicht_Display extends Public_Shortcode_Display {
 	 */
 	protected function html() {
 		if ( 'cursisten' === $this->data['actie'] ) {
-			$this->form()->cursisten()->form_end();
-			return;
+			if ( $this->data['bestuur_rechten'] ) {
+				$this->form()->cursisten_bestuur()->form_end();
+			} else {
+				$this->form()->cursisten_docent()->form_end();
+			}
 		} elseif ( 'indelen' === $this->data['actie'] ) {
 			$this->form()->indelen()->form_end();
 		} elseif ( 'uitschrijven' === $this->data['actie'] ) {
@@ -40,7 +43,7 @@ class Public_Cursus_Overzicht_Display extends Public_Shortcode_Display {
 	 * @return Public_Cursus_Overzicht_Display
 	 * @suppressWarnings(PHPMD.ElseExpression)
 	 */
-	private function cursisten() : Public_Cursus_Overzicht_Display {
+	private function cursisten_bestuur() : Public_Cursus_Overzicht_Display {
 		?>
 		<strong><?php echo esc_html( $this->data['cursus']['code'] . ' ' . $this->data['cursus']['naam'] ); ?></strong>
 		<input type="hidden" name="cursus_id" value="<?php echo esc_attr( $this->data['cursus']['id'] ); ?>">
@@ -50,10 +53,8 @@ class Public_Cursus_Overzicht_Display extends Public_Shortcode_Display {
 				<th>Naam</th>
 				<th>Technieken</th>
 				<th>Betaald</th>
-				<?php if ( $this->data['bestuur_rechten'] ) : ?>
 				<th>Herinner Email</th>
 				<th>Nog niet ingedeeld</th>
-				<?php endif ?>
 			</tr>
 			</thead>
 			<tbody>
@@ -62,51 +63,88 @@ class Public_Cursus_Overzicht_Display extends Public_Shortcode_Display {
 					<td><?php echo esc_html( $cursist['naam'] ); ?></td>
 					<td><?php echo esc_html( $cursist['technieken'] ); ?></td>
 					<?php if ( $cursist['extra'] ) : ?>
-					<td><span class="dashicons dashicons-minus"></span></td>
+						<td><span class="dashicons dashicons-minus"></span></td>
+					<?php elseif ( $cursist['betaald'] ) : ?>
+						<td><span class="dashicons dashicons-yes"></span></td>
 					<?php else : ?>
-					<td><?php echo ( ( $cursist['betaald'] ) ? '<span class="dashicons dashicons-yes"></span>' : '' ); ?></td>
+						<td></td>
 					<?php endif ?>
-
-					<?php if ( $this->data['bestuur_rechten'] ) : ?>
-						<?php if ( $cursist['extra'] ) : ?>
-					<td><span class="dashicons dashicons-minus"></span></td>
-					<td><span class="dashicons dashicons-minus"></span></td>
+					<?php if ( $cursist['extra'] ) : ?>
+						<td><span class="dashicons dashicons-minus"></span></td>
+						<td><span class="dashicons dashicons-minus"></span></td>
 					<?php else : ?>
-					<td><?php echo ( ( $cursist['herinner_email'] ) ? '<span class="dashicons dashicons-yes"></span>' : '' ); ?></td>
-					<td>
-						<?php
-						if ( ! $cursist['ingedeeld'] ) :
-							if ( $cursist['wacht'] || $cursist['wachtlijst'] ) :
-								?>
-							<a href="#" title="indelen" class="kleistad-edit-link"
-								data-id="<?php echo esc_attr( $cursist['code'] ); ?>" 
-								data-actie="<?php echo $cursist['wachtlijst'] ? 'uitschrijven' : 'indelen'; ?>" >
-								<?php echo $cursist['wachtlijst'] ? 'wachtlijst' : 'wacht op factuur'; ?> </a>
-							<?php else : ?>
-							nog niet betaald !
+						<td><?php echo ( ( $cursist['herinner_email'] ) ? '<span class="dashicons dashicons-yes"></span>' : '' ); ?></td>
+						<td>
+							<?php
+							if ( ! $cursist['ingedeeld'] ) :
+								if ( $cursist['wacht'] || $cursist['wachtlijst'] ) :
+									?>
+									<a href="#" title="indelen" class="kleistad-edit-link"
+									data-id="<?php echo esc_attr( $cursist['code'] ); ?>"
+									data-actie="<?php echo $cursist['wachtlijst'] ? 'uitschrijven' : 'indelen'; ?>" >
+									<?php echo $cursist['wachtlijst'] ? 'wachtlijst' : 'wacht op factuur'; ?> </a>
+								<?php else : ?>
+									nog niet betaald !
 								<?php
-							endif;
-						endif
-						?>
-					</td>
-					<?php endif ?>
+								endif;
+							endif
+							?>
+						</td>
 					<?php endif ?>
 				</tr>
 			<?php endforeach ?>
 			</tbody>
 		</table>
 		<br/>
-		<?php if ( $this->data['bestuur_rechten'] ) : ?>
 		<button class="kleistad-button kleistad-download-link" type="button" name="kleistad_submit_cursus_overzicht" data-actie="cursisten" >Download</button>
-		<?php endif ?>
 		<button class="kleistad-button kleistad-download-link" type="button" name="kleistad_submit_cursus_overzicht" data-actie="presentielijst" >Presentielijst</button>
-		<?php if ( $this->data['bestuur_rechten'] ) : ?>
 		<button class="kleistad-button" type="submit" name="kleistad_submit_cursus_overzicht" id="kleistad_herinner" value="herinner_email" data-confirm="Cursisten|weet je zeker dat je nu de herinneringsemail wilt versturen" >Verstuur herinner email</button>
-		<?php endif ?>
 		<button class="kleistad-button kleistad-terug-link" type="button" style="float:right" >Terug</button>
 		<?php
 		return $this;
 	}
+
+	/**
+	 * Render het formulier
+	 *
+	 * @return Public_Cursus_Overzicht_Display
+	 * @suppressWarnings(PHPMD.ElseExpression)
+	 */
+	private function cursisten_docent() : Public_Cursus_Overzicht_Display {
+		?>
+		<strong><?php echo esc_html( $this->data['cursus']['code'] . ' ' . $this->data['cursus']['naam'] ); ?></strong>
+		<input type="hidden" name="cursus_id" value="<?php echo esc_attr( $this->data['cursus']['id'] ); ?>">
+		<table class="kleistad-datatable display" data-paging="false" data-searching="false">
+			<thead>
+			<tr>
+				<th>Naam</th>
+				<th>Technieken</th>
+				<th>Betaald</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php foreach ( $this->data['cursisten'] as $cursist ) : ?>
+				<tr>
+					<td><?php echo esc_html( $cursist['naam'] ); ?></td>
+					<td><?php echo esc_html( $cursist['technieken'] ); ?></td>
+					<?php if ( $cursist['extra'] ) : ?>
+						<td><span class="dashicons dashicons-minus"></span></td>
+					<?php elseif ( $cursist['betaald'] ) : ?>
+						<td><span class="dashicons dashicons-yes"></span></td>
+					<?php else : ?>
+						<td></td>
+					<?php endif ?>
+				</tr>
+			<?php endforeach ?>
+			</tbody>
+		</table>
+		<br/>
+		<button class="kleistad-button kleistad-download-link" type="button" name="kleistad_submit_cursus_overzicht" data-actie="presentielijst" >Presentielijst</button>
+		<button class="kleistad-button kleistad-terug-link" type="button" style="float:right" >Terug</button>
+		<?php
+		return $this;
+	}
+
 
 	/**
 	 * Render het indelen formulier
