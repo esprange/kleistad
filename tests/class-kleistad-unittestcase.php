fb79960e0fc5cae19f4364cc0e3db2ca470668e7
esprange <eric.sprangers@gmail.com>
2021-09-04 18:15:04 +0200

    Uitbreiding testcases

--- tests/class-kleistad-unittestcase.php
+++ tests/class-kleistad-unittestcase.php
@@ -11,11 +11,74 @@ use WP_UnitTestCase;
 use MockPHPMailer;
 
 /**
+ * Mock filter input array function
+ *
+ * @param int  $type      Type input Post of Get.
+ * @param int  $options   Filter opties.
+ * @param bool $add_empty Afwezige keys als null tonen.
+ *
+ * @return mixed
+ */
+function filter_input_array( int $type, $options = FILTER_DEFAULT, bool $add_empty = true ) {
+	// @phpcs:disable
+	if ( INPUT_GET === $type ) {
+		return filter_var_array( $_GET, $options, $add_empty);
+	}
+	return filter_var_array( $_POST, $options, $add_empty );
+	// @phpcs:enable
+}
+
+/**
+ * Mock filter input function
+ *
+ * @param int    $type     Type input Post of Get.
+ * @param string $var_name Variable naam.
+ * @param int    $filter   Filter.
+ * @param int    $options  Filter opties.
+ *
+ * @return mixed
+ */
+function filter_input( int $type, string $var_name, int $filter = FILTER_DEFAULT, $options = 0 ) {
+	// @phpcs:disable
+	if ( INPUT_GET === $type && isset( $_GET[ $var_name ] ) ) {
+		return filter_var( $_GET[ $var_name ], $filter, $options );
+	}
+	if ( isset( $_POST[ $var_name ] ) ) {
+		return filter_var( $_POST[ $var_name ], $filter, $options );
+	}
+	return null;
+	// @phpcs:enable
+}
+
+/**
  * Kleistad Unit test case.
  *
  * phpcs:disable WordPress.NamingConventions
  */
 abstract class Kleistad_UnitTestCase extends WP_UnitTestCase {
+
+	/**
+	 * Hulp functie om de protected frontend functies te kunnen uitvoeren
+	 * Omdat de shortcode class een singleton is wordt een cache opgebouwd voor hergebruik
+	 *
+	 * @param string $shortcode_tag De shortcode die getest wordt.
+	 * @param string $method        De protected class method die moet worden getest.
+	 * @param array  $data          De uit te wisselen data.
+	 * @param array  $atts          De eventuele attributes meegegeven aan de shortcode.
+	 *
+	 * @returns mixed
+	 */
+	protected function public_actie( string $shortcode_tag, string $method, array &$data, array $atts = [] ) {
+		static $shortcodes = [];
+		$class             = Shortcode::get_class_name( $shortcode_tag );
+		$reflection        = new \ReflectionClass( $class );
+		$action            = $reflection->getMethod( $method );
+		$action->setAccessible( true );
+		if ( ! isset( $shortcodes[ $shortcode_tag ] ) ) {
+			$shortcodes[ $shortcode_tag ] = Shortcode::get_instance( $shortcode_tag, $atts );
+		}
+		return $action->invokeArgs( $shortcodes[ $shortcode_tag ], [ &$data ] );
+	}
 
 	/**
 	 * Mock de phpmailer met een extensie
---
+++ tests/class-test-public-inschrijving.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Class Public Inschrijving Test
+ *
+ * @package Kleistad
+ *
+ * @covers \Kleistad\Public_Cursus_Inschrijving
+ */
+
+namespace Kleistad;
+
+/**
+ * Inschrijving test case.
+ */
+class Test_Public_Inschrijving extends Kleistad_UnitTestCase {
+
+	private const CURSUSNAAM = 'Testcursus';
+	private const SHORTCODE  = 'cursus_inschrijving';
+
+	/**
+	 * Formulier data.
+	 *
+	 * @var array $input De ingevoerde data.
+	 */
+	private array $input;
+
+	/**
+	 * Maak een cursist inschrijving aan.
+	 *
+	 * @param bool $wachtlijst Of de cursist op de wachtlijst moet.
+	 * @return Inschrijving De inschrijving.
+	 */
+	private function maak_inschrijving( bool $wachtlijst ) : Inschrijving {
+		$cursist_id              = $this->factory->user->create();
+		$cursist                 = get_user_by( 'ID', $cursist_id );
+		$cursus                  = new Cursus();
+		$cursus->naam            = self::CURSUSNAAM;
+		$cursus->start_datum     = strtotime( '+1 month' );
+		$cursus->inschrijfkosten = 25.0;
+		$cursus->cursuskosten    = 100.0;
+		$cursus->maximum         = 3;
+		$cursus->save();
+		$this->input = [
+			'user_email'      => $cursist->user_email,
+			'email_controle'  => $cursist->user_email,
+			'first_name'      => $cursist->first_name,
+			'last_name'       => $cursist->last_name,
+			'straat'          => 'straat',
+			'huisnr'          => '12345',
+			'pcode'           => '1234AB',
+			'plaats'          => 'plaats',
+			'telnr'           => '0123456789',
+			'cursus_id'       => $cursus->id,
+			'gebruiker_id'    => $cursist_id,
+			'technieken'      => [],
+			'aantal'          => 1,
+			'opmerking'       => '',
+			'betaal'          => 'ideal',
+			'mc4wp-subscribe' => null,
+		];
+		if ( $wachtlijst ) {
+			/**
+			 * Maak eerst de cursus vol zodat er geen ruimte meer is.
+			 */
+			$cursist_ids = $this->factory->user->create_many( $cursus->maximum );
+			for ( $i = 0; $i < 3; $i ++ ) {
+				$inschrijvingen[ $i ] = new Inschrijving( $cursus->id, $cursist_ids[ $i ] );
+				$inschrijvingen[ $i ]->actie->aanvraag( 'ideal' );
+				$order = new Order( $inschrijvingen[ $i ]->geef_referentie() );
+				$inschrijvingen[ $i ]->betaling->verwerk( $order, 25, true, 'ideal' );
+			}
+		}
+
+		/**
+		 * Schrijf nu de cursist in.
+		 */
+		$inschrijving = new Inschrijving( $cursus->id, $cursist_id );
+		return $inschrijving;
+	}
+
+	/**
+	 * Test validate functie.
+	 */
+	public function test_validate() {
+		$this->maak_inschrijving( false );
+		$_POST  = $this->input;
+		$data   = [];
+		$result = $this->public_actie( self::SHORTCODE, 'validate', $data );
+		if ( is_wp_error( $result ) ) {
+			foreach ( $result->get_error_messages() as $error ) {
+				echo $error . "\n"; // phpcs:ignore
+			}
+		}
+		$this->assertFalse( is_wp_error( $result ), 'validate incorrect' );
+	}
+
+	/**
+	 * Test functie stop_wachten.
+	 */
+	public function test_stop_wachten() {
+		$inschrijving = $this->maak_inschrijving( true );
+		$inschrijving->actie->aanvraag( 'ideal' );
+		/**
+		 * Na inschrijving kan de cursist zich uitschrijven van de wachtlijst.
+		 */
+		$data = [ 'input' => $this->input ];
+		$this->public_actie( self::SHORTCODE, 'stop_wachten', $data );
+		$inschrijving2 = new Inschrijving( $inschrijving->cursus->id, $inschrijving->klant_id );
+		$this->assertTrue( $inschrijving2->geannuleerd, 'stop wachten incorrect' );
+	}
+
+	/**
+	 * Test functie indelen na wachten.
+	 */
+	public function test_indelen_na_wachten() {
+		$inschrijving = $this->maak_inschrijving( true );
+		$inschrijving->actie->aanvraag( 'ideal' );
+
+		$inschrijving->cursus->maximum += 1;
+		$inschrijving->cursus->save();
+		Inschrijvingen::doe_dagelijks();
+		/**
+		 * Na inschrijving en als er ruimte is ontstaan in de cursus, dan kan er ingedeeld worden.
+		 */
+		$data = [ 'input' => $this->input ];
+		$this->public_actie( self::SHORTCODE, 'indelen_na_wachten', $data );
+		$order = new Order( $inschrijving->geef_referentie() );
+		$inschrijving->betaling->verwerk( $order, 25, true, 'ideal' );
+		$inschrijving2 = new Inschrijving( $inschrijving->cursus->id, $inschrijving->klant_id );
+		$this->assertTrue( $inschrijving2->ingedeeld, 'indelen na wachten incorrect' );
+	}
+
+	/**
+	 * Test functie inschrijven.
+	 */
+	public function test_inschrijven() {
+		$inschrijving = $this->maak_inschrijving( false );
+		$inschrijving->actie->aanvraag( 'ideal' );
+		$order = new Order( $inschrijving->geef_referentie() );
+		$inschrijving->betaling->verwerk( $order, 25, true, 'ideal' );
+		$inschrijving2 = new Inschrijving( $inschrijving->cursus->id, $inschrijving->klant_id );
+		$this->assertTrue( $inschrijving2->ingedeeld, 'indelen bij inschrijven incorrect' );
+	}
+}
