d65df3e0385331dbde04bada699865e03795080b
esprange <eric.sprangers@gmail.com>
2022-01-12 22:26:47 +0100

    7.1.0

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Donate link: https://www.kleistad.nl
 Tags: kleistad, ceramics
 Requires at least: 4.8.0
 Tested up to: 5.8.3
-Stable tag: 7.0.5
+Stable tag: 7.1.0
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Requires PHP: 7.4
--- kleistad.php
+++ kleistad.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Kleistad
  * Plugin URI:        https://github.com/esprange/kleistad
  * Description:       Een plugin voor vereniging Kleistad. Oven reserveringen, stooksaldo administratie, cursus adminstratie en keramiek recepten.
- * Version:           7.0.5
+ * Version:           7.1.0
  * Author:            Eric Sprangers
  * Author URI:        https://www.kleistad.nl
  * License:           GPL-3.0+
